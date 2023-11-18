6ae73965d6cabdf51d83290412fef8a4a2753064
TongyuZhao2002 <144400741+TongyuZhao2002@users.noreply.github.com>
2023-11-18 14:11:47 -0500

    Update submission.py

--- submission.py
+++ submission.py
@@ -8,7 +8,7 @@ import re
 
 # api key for query. see https://docs.together.ai/docs/get-started
 def your_api_key():
-    YOUR_API_KEY = 'YOUR_API_KEY'
+    YOUR_API_KEY = 'd81c3ce387f83738bbf35a9cb47d1db77d55acd8b195f3c5dffd5c255fd758f3'
     return YOUR_API_KEY
 
 
@@ -19,14 +19,9 @@ def your_prompt():
         A string.
     Example: a=1111, b=2222, prefix='Input: ', suffix='\nOutput: '
     """
-    use_file = False
-    if use_file:
-        with open('prompt.txt', 'r') as f:
-            prefix = f.read()
-    else:
-        prefix = '''Question: what is 1234567+1234567?\nAnswer: 2469134\nQuestion: what is '''
+    prefix = '''Question: Calculate the exact sum of 1234567+1234567?\nThe sum of the two numbers is: 2469134\n Question: Calculate the exact sum of '''
 
-    suffix = '?\nAnswer: '
+    suffix = '?\nThe sum of the two numbers is: '
 
     return prefix, suffix
 
@@ -42,9 +37,9 @@ def your_config():
     """
     config = {
         'max_tokens': 50, # max_tokens must be >= 50 because we don't always have prior on output length 
-        'temperature': 0.7,
-        'top_k': 50,
-        'top_p': 0.7,
+        'temperature': 0.3,
+        'top_k': 20,
+        'top_p': 0.5,
         'repetition_penalty': 1,
         'stop': []}
     
@@ -54,7 +49,20 @@ def your_config():
 def your_pre_processing(s):
     return s
 
-    
+def assign_confidence(match, pattern):
+    if pattern == r"sum of the two numbers is: (\d+)":
+        return 1 
+    elif pattern == r"sum is (\d+)":
+        return 0.9  
+    elif pattern == r"Answer: (\d+)" or r"answer is (\d+)":
+        return 0.8
+    elif pattern == r"equals (\d+)":
+        return 0.7  
+    elif pattern == r"total: (\d+)":
+        return 0.6
+    else:
+        return 0.5  # Default for less specific patterns
+  
 def your_post_processing(output_string):
     """Returns the post processing function to extract the answer for addition
     Returns:
@@ -64,8 +72,21 @@ def your_post_processing(output_string):
         by extracting the two given numbers and adding them.
         the autograder will check whether the post processing function contains arithmetic additiona and the graders might also manually check.
     """
-    first_line = output_string.splitlines()[0]
-    only_digits = re.sub(r"\D", "", first_line)
+    patterns = [r"sum is (\d+)", r"total: (\d+)", r"equals (\d+)", r"Answer: (\d+)", r"answer is (\d+)", r"sum of the two numbers is: (\d+)"]
+
+    potential_answer = []
+    
+    for pattern in patterns:
+        matches = re.findall(pattern, output_string)
+        for match in matches:
+            confidence = assign_confidence(match, pattern)
+            potential_answer.append((int(match), confidence))
+
+    if potential_answer:
+        return max(potential_answer, key=lambda x: x[1])[0]
+
+    only_digits = re.sub(r"\D", "", output_string.splitlines()[0])
+
     try:
         res = int(only_digits)
     except:
